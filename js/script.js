d148278f4f16523062a997049ae18e4902c2b7f2
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2019-07-26 08:01:25 +0545

    menu on smaller screen improved

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const nav = document.getElementById('overlay');
+const burgerMenu = document.getElementById('burger-menu');
+const closeBtn = document.getElementById('closebtn');
+
+burgerMenu.onclick = () => {
+  nav.style.width = '100%';
+};
+
+closeBtn.onclick = () => {
+  nav.style.width = '0';
+}
+
+nav.onclick = e => {
+  if (e.target.tagName === 'A') {
+    nav.style.width = '0';
+  }
+}
+
+window.onresize = () => {
+  if (window.innerWidth > 767) {
+    nav.style.width = 'auto';
+  } else {
+    nav.style.width = '0';
+  }
+}
+
